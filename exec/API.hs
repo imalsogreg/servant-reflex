fab4c6e2f7b1ddd4131e82d337483798a4c93418
Greg Hale <imalsogreg@gmail.com>
2016-02-18 18:53:59 -0500

    More example polish

--- exec/API.hs
+++ exec/API.hs
@@ -5,6 +5,7 @@ module API where
 
 import Servant.API
 
-type API = "getunit" :> Get '[JSON] () 
+-- | API spec for server, client, and docs
+type API = "getunit" :> Get '[JSON] ()
       :<|> "getint"  :> Get '[JSON] Int
       :<|> Raw
--- exec/Example.hs
+++ exec/Example.hs
@@ -19,21 +19,27 @@ url = BaseUrl Http "localhost" 8000 ""
 main :: IO ()
 main = mainWidget run
 
+
 run :: forall t m. MonadWidget t m => m ()
 run = do
-  let (getUnit :<|> getInt :<|> doRaw) = client api (Proxy :: Proxy m) (constDyn url)
-  b  :: Event t () <- button "Get unit"
-  b' :: Event t () <- button "Get int"
-  res :: Event t (Maybe (), XhrResponse) <- getUnit b
-  res' :: Event t (Maybe Int, XhrResponse) <- getInt b'
-  score <- foldDyn (+) 0 (fmapMaybe fst res')
-  r <- holdDyn "Waiting" $ leftmost [fmap (showXhrResponse . snd) res
-                                    ,fmap (showXhrResponse . snd) res'
-                                    ]
-  dynText r
-  el "br" $ return ()
-  text "Total: "
-  display score
+
+  -- Name the computed API client functions
+  let (getUnit :<|> getInt :<|> doRaw) =
+        client api (Proxy :: Proxy m) (constDyn url)
+
+  unitBtn  <- button "Get unit"
+  intBtn   <- button "Get int"
+
+  unitResponse <- getUnit unitBtn
+  intResponse :: Event t (Maybe Int, XhrResponse) <- getInt intBtn
+
+  score <- foldDyn (+) 0 (fmapMaybe fst intResponse)
+
+  r <- holdDyn "Waiting" $
+         leftmost [fmap (showXhrResponse . snd) unitResponse
+                  ,fmap (showXhrResponse . snd) intResponse
+                  ]
+  dynText r >> el "br" (return ()) >> text "Total: " >> display score
 
 showXhrResponse :: XhrResponse -> String
 showXhrResponse (XhrResponse stat stattxt rbmay rtmay) =

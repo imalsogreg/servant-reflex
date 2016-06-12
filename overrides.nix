1d2a901ecf5943fc9719fdb39201f0aaf4358762
Greg Hale <imalsogreg@gmail.com>
2016-06-12 09:56:17 -0400

    Deps and build cleanup

--- overrides.nix
+++ overrides.nix
@@ -5,6 +5,6 @@ let
 in reflex-platform.ghcjs.override {
   overrides = self: super: {
     reflex-dom-contrib = pkgs.haskell.lib.dontCheck (self.callPackage deps/reflex-dom-contrib {});
-    servant = self.callPackage (reflex-platform.cabal2nixResult deps/servant/servant) {};
+    servant = pkgs.haskell.lib.dontCheck (self.callPackage (reflex-platform.cabal2nixResult deps/servant/servant) {});
   };
 }

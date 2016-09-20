28d0c3332863fef4c1ea0a86d32dfef38e08605c
Greg Hale <imalsogreg@gmail.com>
2016-09-20 09:33:02 -0400

    Build testserver w/ reflex platform and start servers early

--- overrides-ghc.nix
+++ overrides-ghc.nix
@@ -2,8 +2,19 @@
 let 
   pkgs = import <nixpkgs> {};
   reflex-platform = import deps/reflex-platform {};
+  c2n = reflex-platform.cabal2nixResult;
 in reflex-platform.ghc.override {
   overrides = self: super: {
-    reflex-dom = pkgs.haskell.lib.dontCheck (self.callPackage deps/reflex-dom {});
+    reflex-dom   = pkgs.haskell.lib.dontCheck (self.callPackage deps/reflex-dom {});
+    servant-snap = pkgs.haskell.lib.dontCheck (self.callPackage (c2n deps/servant-snap) {});
+    snap         = pkgs.haskell.lib.dontCheck (self.callPackage (c2n deps/servant-snap/deps/snap) {});
+    snap-server  = pkgs.haskell.lib.dontCheck (self.callPackage (c2n deps/servant-snap/deps/snap/deps/snap-server) {});
+    io-streams  = pkgs.haskell.lib.dontCheck (self.callPackage (c2n deps/servant-snap/deps/snap/deps/io-streams) {});
+    io-streams-haproxy  = pkgs.haskell.lib.dontCheck (self.callPackage (c2n deps/servant-snap/deps/snap/deps/io-streams-haproxy) {});
+    heist  = pkgs.haskell.lib.dontCheck (self.callPackage (c2n deps/servant-snap/deps/snap/deps/heist) {});
+    xmlhtml  = pkgs.haskell.lib.dontCheck (self.callPackage (c2n deps/servant-snap/deps/snap/deps/xmlhtml) {});
+    snap-core  = pkgs.haskell.lib.dontCheck (self.callPackage (c2n deps/servant-snap/deps/snap/deps/snap-core) {});
+    servant  = pkgs.haskell.lib.dontCheck (self.callPackage (c2n deps/servant/servant) {});
+    http-api-data = pkgs.haskell.lib.dontCheck (self.callPackage (c2n deps/http-api-data) {});
   };
 }

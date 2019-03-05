d6e25032440fd4995338ebdaf12fee98310dc167
Jappie Klooster <jappieklooster@hotmail.com>
2019-03-05 13:45:04 +0100

    Use predicate for response handling
    This allows users to speicify how they want to deal with
    status codes and responses themselves.
    Which allows it to be used in most situations.

--- src/Servant/Reflex/Response.hs
+++ src/Servant/Reflex/Response.hs
@@ -3,9 +3,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 -- | Response handlers
+--   Usefull for 'clientWithOptsAndResultHandler'
 module Servant.Reflex.Response
   ( reloadOnAPIError
+  , reloadOnResponse
   , RefreshSupport
+  , isAuthErr
+  , is4xxErr
+  , isDecodeError
   ) where
 
 import           Control.Monad               (join, void)
@@ -15,35 +20,64 @@ import           JSDOM.Generated.Document    (getLocation)
 import           JSDOM.Types                 (Location, liftDOM)
 import           Language.Javascript.JSaddle (MonadJSM, ( # ))
 import           Reflex
-import           Reflex.Dom
+import           Reflex.Dom.Builder.Class
+import           Reflex.Dom.Widget.Basic
+import           Reflex.Dom.Xhr
 import           Servant.Reflex
 
-is4xx :: ReqResult () a -> Bool
-is4xx = maybe False (\req -> (_xhrResponse_status req < fromInteger 500) && (_xhrResponse_status req >= 400)) . response
-
-isDecodeError  :: ReqResult () a -> Bool -- TODO find a better way of detecting this, this seems dumb
-isDecodeError  =
-  maybe False (Text.isInfixOf "Error in $") . reqFailure
-
 -- | Alias for constraints that can refresh
-type RefreshSupport t m = (MonadJSM m, MonadHold t m, DomBuilder t m, SupportsServantReflex t m)
+type RefreshSupport t m
+   = (MonadJSM m, MonadHold t m, DomBuilder t m, SupportsServantReflex t m)
 
--- | Reloads page and clears the cache on any 4xx response or on any
---   decoding errors.
+is4xxErr :: ReqResult () a -> Bool
+is4xxErr =
+  maybe
+    False
+    (\req ->
+       (_xhrResponse_status req < fromInteger 500) &&
+       (_xhrResponse_status req >= 400)) .
+  response
+
+isAuthErr :: ReqResult () a -> Bool
+isAuthErr =
+  maybe
+    False
+    (\req ->
+       let stat = _xhrResponse_status req
+       in (stat == fromInteger 401) &&
+           (stat == fromInteger 402) && (stat == fromInteger 407)) .
+  response
+
+isDecodeError :: ReqResult () a -> Bool -- TODO find a better way of detecting this, is there no typesafe decode error?
+isDecodeError = maybe False (Text.isInfixOf "Error in $") . reqFailure
+
+-- | Reloads page and clears the cache on any 4xx response which is not auth related
+--   or on any decoding errors.
 --   Most endpoints want to have this.
 --   These errors mean the client code is probably outdated.
---   If so a clear cache refresh will fix the issue.
---   We no longer care about the JS growing stale, any 4xx response will just refresh it.
---   It also saves time in that we don't have to bother presenting good error messages for 4xx respones.
---   The tradeoff is that our api should only return a 4xx if it thinks the client is broken.
---   If developer wants to show a serious error message to the user he shouldn't rely on status codes
---   anyway because they're not type safe (in servant).
---   Status codes also don't provide good user experience,
---   *or* dev experience. A code is simply not human friendly.
-reloadOnAPIError :: (MonadJSM m, RefreshSupport t m) => Event t (ReqResult () a) -> m (Event t (ReqResult () a))
-reloadOnAPIError reqResult = do
-  void $ widgetHold blank $ ffor reqResult $
-    \req -> if is4xx req || isDecodeError req then clearCacheReload else pure ()
+--   This assumes client errros aren't used in your app logic,
+--   for more flexibility see 'reloadOnResponse'
+reloadOnAPIError ::
+     (MonadJSM m, RefreshSupport t m)
+  => Event t (ReqResult () a)
+  -> m (Event t (ReqResult () a))
+reloadOnAPIError =
+  reloadOnResponse
+    (\req -> (is4xxErr req && (not $ isAuthErr req)) || isDecodeError req)
+
+-- | Reload and clears the cache if the predicate returns true
+reloadOnResponse ::
+     (MonadJSM m, RefreshSupport t m)
+  => (ReqResult () a -> Bool)
+  -> Event t (ReqResult () a)
+  -> m (Event t (ReqResult () a))
+reloadOnResponse predic reqResult = do
+  void $
+    widgetHold blank $
+    ffor reqResult $ \req ->
+      if predic req
+        then clearCacheReload
+        else pure ()
   pure reqResult
 
 -- | <https://developer.mozilla.org/en-US/docs/Web/API/Location.reload Mozilla Location.reload documentation>
@@ -55,9 +89,9 @@ reload' self bool = liftDOM $ void $ self # funName $ bool
 
 -- | Refresh and clear the browser cache, eg all assets will be reloaded.
 --   this could be used for example once you received a client error,
---   it probably means you've outdated code.
+--   it probably means you've got outdated code.
 clearCacheReload :: MonadJSM m => m ()
 clearCacheReload = do
-      mayWin <- currentDocument
-      loc <- sequence $ getLocation <$> mayWin
-      maybe (pure ()) (flip reload' True) $ join loc
+  mayWin <- currentDocument
+  loc <- sequence $ getLocation <$> mayWin
+  maybe (pure ()) (flip reload' True) $ join loc

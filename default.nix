8b5d38d719a493ff7353b7daa58f9bb58a90d1c7
Jappie Klooster <jappieklooster@hotmail.com>
2019-03-05 13:52:02 +0100

    Forgot scientific

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, aeson, base, bytestring, case-insensitive, containers
 , data-default, exceptions, ghcjs-dom, http-api-data, http-media
 , jsaddle, jsaddle-dom, mtl, network-uri, reflex, reflex-dom
-, reflex-dom-core , safe, servant, servant-auth, stdenv
+, reflex-dom-core , safe, scientific, servant, servant-auth, stdenv
 , string-conversions, text , transformers
 }:
 mkDerivation {

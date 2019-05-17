4c06b60eb7973e8a92a7df1e52b466bdee10533b
Matthew Bauer <mjbauer95@gmail.com>
2019-05-17 15:36:57 -0400

    Regen default.nix

--- default.nix
+++ default.nix
@@ -1,29 +1,25 @@
 { mkDerivation, aeson, base, bytestring, case-insensitive
 , containers, data-default, exceptions, ghcjs-dom, http-api-data
-, http-media, jsaddle, mtl, network-uri, reflex, reflex-dom
-, reflex-dom-core, safe, scientific, servant, servant-auth, stdenv
-, string-conversions, text, transformers, megaparsec, wreq
+, http-media, jsaddle, mtl, network-uri, reflex, reflex-dom-core
+, safe, servant, servant-auth, servant-checked-exceptions, stdenv
+, string-conversions, text, transformers
 }:
 mkDerivation {
   pname = "servant-reflex";
   version = "0.3.4";
+  configureFlags = [ "-fexample" ];
   src = builtins.filterSource
         (path: type:
         baseNameOf path != "result"
-        && baseNameOf path != "nix" 
-        ) ./.;
-  configureFlags = [ "-fexample" ];
+        && baseNameOf path != "nix") ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    base bytestring case-insensitive containers data-default exceptions
-    ghcjs-dom http-api-data http-media jsaddle mtl network-uri reflex
-    reflex-dom-core safe servant servant-auth string-conversions text
-    transformers megaparsec wreq
+    aeson base bytestring case-insensitive containers data-default
+    exceptions ghcjs-dom http-api-data http-media jsaddle mtl
+    network-uri reflex reflex-dom-core safe servant servant-auth
+    servant-checked-exceptions string-conversions text transformers
   ];
-  executableHaskellDepends = [
-    aeson base reflex reflex-dom scientific servant text
-  ];
-  description = "Servant reflex API generator";
+  description = "servant API generator for reflex apps";
   license = stdenv.lib.licenses.bsd3;
 }
